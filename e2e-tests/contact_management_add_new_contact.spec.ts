87ab60a9eb20de77fee168e835f8712561431cd7
Regine <reginefrencillo@sorsu.edu.ph>
2025-09-08 20:48:36 +0800

    revised add contact(CRUD) w/o profile pic and no data on contact access)

--- e2e-tests/contact_management_add_new_contact.spec.ts
+++ e2e-tests/contact_management_add_new_contact.spec.ts
@@ -26,31 +26,68 @@ test.describe('impersonate Customer and navigate to Contact Management', () => {
     await page.locator('a').filter({ hasText: 'Contacts' }).click();
    });
 
-  test('add new contact without uploading pic and no data on Contact Access.', async ({ page }) => {
-    firstName = faker.person.firstName();
-    lastName = faker.person.lastName();
-    email = faker.internet.email();
-    phone = faker.helpers.replaceSymbols('1 (###) ###-####');
-    //Verify if it can add new contact by filling all the details but without contact access and profile pic.
-    await page.getByText('Add New Contact').click();
+    test('CRUD for contact management without uploading picture and no data on Contact Access.', async ({ page }) => {
+      firstName = 'Sam';
+      lastName = 'Smith';
+      email = 'sam@smith.com';
+      phone = '1 (555) 123-4567';
 
-    await page.getByPlaceholder('First Name').click();
-    await page.getByPlaceholder('First Name').type(firstName, { delay: 40 });
+      await test.step('Contact list- add new contact without uploading pic and no data on Contact Access.', async () => {
+        //firstName = faker.person.firstName();
+        //lastName = faker.person.lastName();
+        //email = faker.internet.email();
+        //phone = faker.helpers.replaceSymbols('1 (###) ###-####');
 
-    await page.getByPlaceholder('Last Name').click();
-    await page.getByPlaceholder('Last Name').type(lastName, { delay: 40 });
+        //Verify if it can add new contact by filling all the details but without contact access and profile pic.
+        await page.getByText('Add New Contact').click();
 
-    await page.getByPlaceholder('example@domain.com').click();
-    await page.getByPlaceholder('example@domain.com').type(email, { delay: 40 });
+        await page.getByPlaceholder('First Name').click();
+        await page.getByPlaceholder('First Name').type(firstName, { delay: 40 });
 
-    await page.getByPlaceholder('1 (702) 123-').click();
-    await page.getByPlaceholder('1 (702) 123-').type(phone,{ delay: 40 });
+        await page.getByPlaceholder('Last Name').click();
+        await page.getByPlaceholder('Last Name').type(lastName, { delay: 40 });
 
-    await page.locator('div').filter({ hasText: /^English \(US\)$/ }).nth(2).click();
-    await page.locator('div').filter({ hasText: /^Select Group$/ }).nth(2).click();
-    await page.pause();
-    await page.getByText('Save').click();
-    await expect(page.getByText('Contact added successfully')).toBeVisible();
-  })
+        await page.getByPlaceholder('example@domain.com').click();
+        await page.getByPlaceholder('example@domain.com').type(email, { delay: 40 });
+
+        await page.getByPlaceholder('1 (702) 123-').click();
+        await page.getByPlaceholder('1 (702) 123-').type(phone,{ delay: 40 });
+
+        await page.locator('div').filter({ hasText: /^English \(US\)$/ }).nth(2).click();
+        await page.locator('div').filter({ hasText: /^Select Group$/ }).nth(2).click();
+        await page.getByText('Save').click();
+        await expect(page.getByText('Contact added successfully')).toBeVisible();
+      })
+
+    await test.step('View Added contact and verify details', async () => {
+      await page.getByPlaceholder('Search Contact Name').click();
+      await page.getByPlaceholder('Search Contact Name').type(firstName + ' ' + lastName, { delay: 40 });
+      await page.getByText(firstName + ' ' + lastName).click();
+      await page.waitForTimeout(2000);
+    });
+
+    await test.step('Verify if it can edit contact details', async () => {
+      await page.locator('tbody button').first().click();
+      await page.getByPlaceholder('example@domain.com').click();
+      await page.waitForTimeout(2000);
+      await page.getByPlaceholder('example@domain.com').fill('sam@email.com');
+      await page.getByText('Save').click();
+
+      //wait for confirmation message
+      await expect(page.getByText('The contact has been updated successfully.')).toBeVisible();
+    });
+
+    await test.step('Verify if it can view the edited contact and can delete the contact', async () => {
+      await page.getByPlaceholder('Search Contact Name').click();
+      await page.getByPlaceholder('Search Contact Name').type(firstName + ' ' + lastName, { delay: 40 });
+      await page.getByText(firstName + ' ' + lastName).click();
+      await page.waitForTimeout(2000);
+
+      //Delete the contact
+      await page.locator('tbody button').nth(1).click();
+      await page.getByText('Confirm Delete').click();
+      await expect(page.getByText('Contact deleted successfully.')).toBeVisible(); 
+    });
+  });
 
 });
